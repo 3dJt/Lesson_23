8742e95d9dc69d777969471b0a1605c7bfe48e06
Anton <antonkalash037@mail.ru>
2024-03-13 19:06:46 +0300

    Job

---
+++ parser.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://dedmorozural.ru/"
+responce = requests.get(url)
+print(responce.status_code)
+# print(responce.text)
+a = responce.text
+
+soup = BeautifulSoup(a, 'html.parser')
+
+print(soup.title)
+print(type(soup.title))
+
+print(soup.a)
+print(type(soup.a.string))
+
+print(soup.a.get("href"))
+print('-' * 25)
+
+# images_tags = soup.find_all('img')
+# for images_tags in images_tags:
+#     print(images_tags)
+
+# print('\n------------------------- Все ссылки -------------------------\n')
+
+# a_tags = soup.find_all('a')
+# for a_tags in a_tags:
+#     print(a_tags)
+
+big_body_div = soup.find('div', class_ = 'modulebody1')
+print(big_body_div)
